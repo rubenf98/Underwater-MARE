7066dff138213ef5ae6a64aae518932137a4a313
FranciscoS00 <53246322+FranciscoS00@users.noreply.github.com>
2024-04-16 15:36:18 +0100

    hero image

--- src/views/Homepage/components/Home/Home.jsx
+++ src/views/Homepage/components/Home/Home.jsx
@@ -3,11 +3,6 @@ import styles from "./Home.module.css";
 function Home() {
   return (
     <div className={styles.container} id="home">
-      <img
-        className={styles.backgroundImage}
-        src="/assets/landing.webp"
-        alt="Landing page image of a diver doing an underwater visual survey census"
-      />
       <div className={styles.textContainer}>
         <h1 className={styles.outlineTitle}>THE OCEAN</h1>
         <h1>TO EXPLORE</h1>
